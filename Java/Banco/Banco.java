97315770473c3617ef5df3ec1a71dc0f50dac3aa
DesignMA <designsma.contact@gmail.com>
2024-08-26 14:56:46 -0600

    "Removed Scanner instances and input handling from Banco and Cuenta classes, instead passing input values as method parameters."

--- Java/Banco/Banco.java
+++ Java/Banco/Banco.java
@@ -1,15 +1,10 @@
 package Banco; //Carpeta (paquete)
-import java.util.Scanner;
-
 public class Banco { //Clase principal
 
     public static void main ( String [] args) {
-        Scanner in = new Scanner( System.in );
-        String nombre;
-
         //Instancia de la clase CuentaBancaria (Objetos)
         Cuenta c1 = new Cuenta(); //Identidad
-        Cuenta c2;
+        Cuenta c2 = new Cuenta("Juan Capulin");
         c1.setSaldo(9999);
         c1.setTitular("Paco Antonio");
         System.out.println(c1.getNumeroCuenta());
@@ -17,15 +12,11 @@ public class Banco { //Clase principal
         System.out.println(c1.getSaldo());
         System.out.println(c1.toString());
 
-        System.out.println("\nIngrese el nombre del titular de la cuenta: ");
-        nombre = in.nextLine();
-        c2 = new Cuenta(nombre); //objeto
         c2.datosCuenta(); //Estado
-        c2.depositar(); //Comportamiento
-        c2.retirar();
+        c2.depositar(140.5f); //Comportamiento
+        c2.retirar(150.0f);
+        c2.retirar(100.0f);
         c2.datosCuenta(); //Comportamiento
 
-        in.close();
-        //Hola estoy editando
     }
 }
