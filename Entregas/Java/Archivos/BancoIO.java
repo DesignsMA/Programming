5073706664d5b4866ed5fc09ec13e94fe3f4056c
DesignMA <designsma.contact@gmail.com>
2024-11-10 13:52:13 -0600

    "Added "Archivos" to Entregas\JAVA"

---
+++ Entregas/Java/Archivos/BancoIO.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/* https://www.geeksforgeeks.org/file-class-in-java/ | Notas manejo de archivos */
+/*
+• public static void leerArchivo(String nombreArchivo, Banco banco) 
+Este método debe crear un objeto Scanner que lea del archivo cuyo nombre se especifica en el 
+paso de parámetros. Mientras haya datos, el método leerArchivo() debe invocar 
+repetidamente al método leerCuenta(), el cual regresará un objeto de tipo CuentaBancaria. El 
+método leerArchivo() deberá agregar la cuenta bancaria al banco proporcionado en el paso de 
+parámetros. 
+
+• public static CuentaBancaria leerCuenta(Scanner sc) 
+Este método se usará para leer los datos de una cuenta bancaria. Se debe recibir por 
+parámetros un objeto de tipo Scanner, con el cual se leerá el número de cuenta (int) y el 
+balance (double) del archivo. Este método deberá crear un nuevo objeto de tipo 
+CuentaBancaria y retornarlo al método que lo invocó. */
+public class BancoIO {
+
+    public static void leerArchivo(String nombreArchivo, Banco banco) {
+        try {
+            Scanner sc = new Scanner(new File(nombreArchivo));
+        } catch (FileNotFoundException e) { // Atrapando EXCEPCION
+            // TODO: handle exception
+        }
+
+    }
+
+}
