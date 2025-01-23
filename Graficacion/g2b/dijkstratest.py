3e5475fc50875b682da9247b6d2c54e8e2ac631b
DesignMA <designsma.contact@gmail.com>
2025-01-23 16:40:16 -0600

    "Labels added."

--- Graficacion/g2b/dijkstratest.py
+++ Graficacion/g2b/dijkstratest.py
@@ -1,12 +1,14 @@
 from collections import namedtuple, deque
 from pprint import pprint as pp
+import heapq #Cola de prioridad para implementar el algoritmo
  
  
-inf = float('inf')
-Edge = namedtuple('Edge', ['start', 'end', 'cost'])
+inf = float('inf') #infinito
+Edge = namedtuple('Edge', ['start', 'end', 'cost']) #lados
  
 class Graph():
-    def __init__(self, edges):
+    def __init__(self, edges): #recibe arreglo de tuplas
+        #desempaca los datos contenidos en las tuplas y los manda como argumentos a Edge
         self.edges = [Edge(*edge) for edge in edges]
         self.vertices = {e.start for e in self.edges} | {e.end for e in self.edges}
  
@@ -21,16 +23,20 @@ class Graph():
             neighbours[start].add((end, cost))
             neighbours[end].add((start, cost))
 
+        #pp(neighbours)
+ 
         while q:
+            # pp(q)
             u = min(q, key=lambda vertex: dist[vertex])
             q.remove(u)
             if dist[u] == inf or u == dest:
                 break
             for v, cost in neighbours[u]:
                 alt = dist[u] + cost
-                if alt < dist[v]:
+                if alt < dist[v]:                                  # Relax (u,v,a)
                     dist[v] = alt
                     previous[v] = u
+        #pp(previous)
         s, u = deque(), dest
         while previous[u]:
             s.appendleft(u)
--- Graficacion/g2b/pathfinder.py
+++ Graficacion/g2b/pathfinder.py
@@ -3,10 +3,9 @@ import subprocess
 import math
 import dijkstra
 from collections import namedtuple, deque
-from pprint import pprint as pp
 
 points = []
-for i in range(4): # filas
+for i in reversed(range(4)): # filas
     for j in range(4): # columnas
         points.append( spline.Point(i, j) ) # generar puntos
         
@@ -31,8 +30,11 @@ with open('adj.txt', 'w') as f:
             u += 1
 
 graph = dijkstra.Graph(adj) # crear grafo
+path = []
+pathdijkstra = graph.dijkstra("0", "15")
 print (adj)
-pp(graph.dijkstra("12", "3"))
+print(pathdijkstra)
+
         
 with open('matrix.txt', 'w') as f:
         i = 0
@@ -42,8 +44,7 @@ with open('matrix.txt', 'w') as f:
             i +=1
             
 with open('path.txt', 'w') as f:
-        path = []
-        for x in graph.dijkstra("12", "3"): #retorna el camino
+        for x in pathdijkstra:
             print(x)
             p = points[int(x)]
             path.append(p)
@@ -52,10 +53,25 @@ with open('path.txt', 'w') as f:
 with open('pathLines.txt', 'w') as f:
         for i in range(len(path)-1): #para cubrir todos los puntos
                 f.write(f"{path[i].x} {path[i].y} {path[i+1].x} {path[i+1].y}\n")
-        
 
 spline.splinePoints(path) #Generar puntos de la curva de bezier
+
+
+with open('bezier_data.txt', 'r') as f:
         
+        for i in range( int(spline.resolution/2) -1):
+              f.readline() #leer hasta la mitad
+        center = f.readline()
+
+        pathtxt = "Path="
+        for i in range(len(pathdijkstra)-1):
+              pathtxt += pathdijkstra[i] + "-"
+        
+        pathtxt += pathdijkstra[i]
+
+with open('pathLabel.txt', 'w') as f:
+      f.write(f"{center.split(" ")[0]} {center.split(" ")[1].strip("\n")} {pathtxt}")
+
 command = "gnuplot -e \"set terminal pngcairo;\
            set output \'graph.png\';\
            set zeroaxis;\
@@ -64,11 +80,12 @@ command = "gnuplot -e \"set terminal pngcairo;\
            set border 0;\
            plot \'adj.txt\' using 1:2:($3-$1):($4-$2) title \'\' with vectors nohead lc rgb \'#2f95fd\',\
            \'adj.txt\' using 5:6:7 with labels title \'\' font \'Arial,7\',\
-           'matrix.txt' using 1:2:3 with points title \'Matrix\' ps 3 pt 7 lc rgb '#6100ff',\
+           'matrix.txt' using 1:2:3 with points title \'Graph\' ps 3 pt 7 lc rgb '#6100ff',\
            'bezier_data.txt' using 1:2 with points title \'Bézier Curve\' ps 0.2 pt 7 lc rgb '#ff5353',\
             \'pathLines.txt\' using 1:2:($3-$1):($4-$2) title \'\' with vectors nohead lc rgb \'#ee5252\' lw 1.7,\
-            \'path.txt\' using 1:2:3 with points title \'Path\' ps 3 pt 7 lc rgb \'#ff5353\',\
-            \'matrix.txt\' using 1:2:4 with labels title \'\' tc rgb \'#ffffff\' font \'DejaVu Sans,8\'\"\
+            \'path.txt\' using 1:2:3 with points title \'Control\' ps 3 pt 7 lc rgb \'#ff5353\',\
+            \'pathLabel.txt\' using 1:2:3 with labels title \'\' tc rgb \'#171717\' font \'DejaVu Sans Bold,9\',\
+            \'matrix.txt\' using 1:2:4 with labels title \'\' tc rgb \'#ffffff\' font \'DejaVu Sans Bold,7\'\"\
             "
             
 subprocess.run(command, shell=True)
