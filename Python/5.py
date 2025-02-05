d3be5fa52ce23a03b0629660e10e66a02f21036c
DesignMA <designsma.contact@gmail.com>
2025-02-05 14:07:19 -0600

    "5.py added."

---
+++ Python/5.py
@@ -0,0 +1,18 @@
+import sys
+
+f =  open("notes.txt", "w+") #abrir archivo
+f.write("\t\t--NOTES--\n")
+print("\t\tPress Ctrl+Z to exit:")
+while True:
+    try:
+        line = input()[:50] #leer cadena de usuario, solo los primeros 50 caracteres
+
+        for i in range(51-len(line)):
+            line += '-'
+        line += '\n'
+
+        f.write(line)
+    except EOFError:
+        print("\nExiting...\n'notes.txt' was created.")
+        f.close()
+        exit()
