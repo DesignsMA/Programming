29644b12fbc0edbc9de7fce7926484cfabfe0790
DesignMA <designsma.contact@gmail.com>
2025-02-20 14:56:10 -0600

    "Final build."

--- tpTo/src/main/java/com/designma/tpto/TpToMod.java
+++ tpTo/src/main/java/com/designma/tpto/TpToMod.java
@@ -7,11 +7,11 @@ import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = TpToMod.MODID, name = TpToMod.NAME, version = TpToMod.VERSION)
+@Mod(modid = TpToMod.MODID, useMetadata = true, name = TpToMod.NAME, version = TpToMod.VERSION)
 public class TpToMod {
     public static final String MODID = "tpto";
     public static final String NAME = "TpTo";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
