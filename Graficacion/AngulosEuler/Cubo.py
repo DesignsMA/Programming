a556577d6e082466ba4c904295ad403b8d200364
DesignMA <designsma.contact@gmail.com>
2025-03-01 15:39:28 -0600

    "Creating cube faces."

--- Graficacion/AngulosEuler/Cubo.py
+++ Graficacion/AngulosEuler/Cubo.py
@@ -1,41 +1,54 @@
 from bezierSurface import *
 
-def graph(mesh, m, n):
-        ptx = np.array([point.x for point in mesh])
-        pty = np.array([point.y for point in mesh])
-        ptz = np.array([point.z for point in mesh])
+def graphCube(l,w,h, subdivisions):
+    # Definir los puntos de las 6 caras del cubo
+    caras =[ np.array([[Point(0, 0, 0), Point(0, w, 0)],
+                             [Point(l, 0, 0), Point(l, w, 0)]]), 
+            
+            np.array([[Point(0, 0, h), Point(0, w, h)],
+                             [Point(l, 0, h), Point(l, w, h)]]),
+            
+            np.array([[Point(0, w, 0), Point(0, w, h)],
+                              [Point(l, w, 0), Point(l, w, h)]]),
+            
+            np.array([[Point(0, 0, 0), Point(0, 0, h)],
+                              [Point(l, 0, 0), Point(l, 0, h)]]),
+            
+            np.array([[Point(0, 0, 0), Point(0, 0, h)],
+                             [Point(0, w, 0), Point(0, w, h)]]),
+            
+            np.array([[Point(l, 0, 0), Point(l, 0, h)],
+                           [Point(l, w, 0), Point(l, w, h)]])
+    ]
+    # Crear la figura y el eje 3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    for cara in caras:
+        mesh = BezierSurface(cara, subdivisions)
+        mesh.generateMesh()
         
-        fig = plt.figure() #generar figura
-        ax = fig.add_subplot(projection='3d') #añadir subplot
-        shapeM = m
-        shapeN = n
-        ptX = ptx.reshape(shapeM,shapeN)
-        ptY = pty.reshape(shapeM,shapeN)
-        ptZ = ptz.reshape(shapeM,shapeN)
-        
+        ptx = np.array([point.x for point in mesh.mesh])
+        pty = np.array([point.y for point in mesh.mesh])
+        ptz = np.array([point.z for point in mesh.mesh])        
+                
+        ptX = ptx.reshape(subdivisions+1, subdivisions+1)
+        ptY = pty.reshape(subdivisions+1, subdivisions+1)
+        ptZ = ptz.reshape(subdivisions+1, subdivisions+1)
+
         # Aplicar gradiente de colores a la superficie
-        ax.plot_surface(ptX, ptY, ptZ, cmap='viridis', rstride=1, cstride=1, alpha=0.7)
-        
+        ax.plot_surface(ptX, ptY, ptZ, cmap='viridis',rstride=1, cstride=1, alpha=1)       
         # Trazar la malla de alambre
-        ax.plot_wireframe(ptX, ptY, ptZ, color='black', linewidth=0.5)
+        ax.plot_wireframe(ptX, ptY, ptZ, color='black', linewidth=0.2)
         
-        ax.scatter(ptx, pty, ptz, c='black', s=5, depthshade=False) #graficar puntos
+        ax.scatter(ptx, pty, ptz, c='black', s=3, depthshade=False) #graficar puntos
+        
+    # Configurar los ejes
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
 
-        # Configuraciones adicionales
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        plt.show()
-
-# cara inferior
-mallado = BezierSurface(np.array( [[Point(0, 0, 0), Point(0, 20, 0)],
-                                  [Point(-20, 0, 0), Point(-20, 20, 0)]] ))
-mallado.generateMesh()
-cuboMesh = mallado.mesh
-
-mallado.surfacePoints= np.array( [[Point(0, 0, 0), Point(0, 0, 1)],
-                                  [Point(-20, 0, 0), Point(-20, 0, 1)]] )
-
-mallado.generateMesh()
-cuboMesh = set(mallado.mesh + cuboMesh)
-graph(cuboMesh, 11, 22)
+    # Mostrar la gráfica
+    plt.show()# cara inferior
+    
+graphCube(20, 20, 1, 10)
