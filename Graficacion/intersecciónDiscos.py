54000c0e18d1a72695cc35280e310f34404b6ec8
DesignMA <designsma.contact@gmail.com>
2025-03-11 19:47:15 -0600

    "Finishing disc difference."

--- Graficacion/intersecciónDiscos.py
+++ Graficacion/intersecciónDiscos.py
@@ -6,12 +6,19 @@ def perteneceADisco(punto, xc, yc, zc, r):
     if not np.isclose(punto.z, zc, atol=1e-6):
         print("El disco dos no está en el mismo plano que el disco uno")
         return False  # El punto no está en el plano del disco
+        # ecuación del disco, si la distancia cuadra es menor o igual a cero, pertenece al disco
     return (punto.x - xc)**2 + (punto.y - yc)**2 - r**2 <= 0 # Debe estar dentro o en el borde
 
 def interseccionDisco(d1: DiscMesh, d2: DiscMesh):
     " Retorna los puntos de intersección entre el disco uno y dos "
     res = [ p for p in d1.mesh if perteneceADisco(p, d2.center.x, d2.center.y, d2.center.z, d2.radio)]
     return res
+
+def diferenciaDisco(d1: DiscMesh, d2: DiscMesh):
+    """ Retorna los puntos de d1 que NO están en d2 """
+    diferencia = [p for p in d1.mesh if not perteneceADisco(p, d2.center.x, d2.center.y, d2.center.z, d2.radio)]
+    return diferencia
+
 
 def graph(ax, color, mesh, size: int=1, alpha: float=1):
     ptx = np.array([point.x for point in mesh])
@@ -35,7 +42,10 @@ disc2.generateMesh()
 
 
 res = set(interseccionDisco(disc1,disc2))
-graph(ax, 'black', disc1.mesh+disc2.mesh, 0.5, alpha=0.15)
-graph(ax,'#ff5353', res, 2)
+res_diff1 = set( diferenciaDisco(disc1,disc2)) # estan en d1 pero no en d2
+res_diff2 = set( diferenciaDisco(disc2,disc1)) # estan en d2 pero no en d1
+graph(ax, 'black', res_diff1, 1, alpha=1)
+graph(ax, 'black', res_diff2, 1, alpha=1)
+graph(ax,'#ff5353', res, 1)
 plt.show()
 
